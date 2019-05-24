30c8b6a83df37bff71b4cf3bb44d65f55bd82fbf
jaeju.jang <jjj0611@gmail.com>
2019-05-24 10:45:18 +0900

    [ADD] computed property에 대해서 학습하였습니다.

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name:'',
-    age:''
+    age: 20,
+    a: 0,
+    b: 0
   },
   methods: {
-    logName: function() {
-      console.log("You entered your name");
+    /* addToA: function() {
+      console.log("addToA");
+      return this.age + this.a;
     },
-    logAge: function() {
-      console.log("You entered your age");
+    addToB: function() {
+      console.log("addToB");
+      return this.age + this.b;
+    } */
+  },
+  computed: {
+    addToA: function() {
+      console.log("addToA");
+      return this.age + this.a;
+    },
+    addToB: function() {
+      console.log("addToB");
+      return this.age + this.b;
     }
   }
 });
