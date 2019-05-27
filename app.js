74bff88dc92b89c290b8fd1855f47eb8ca5df4a4
jaeju.jang <jjj0611@gmail.com>
2019-05-27 09:48:23 +0900

    [AAD] Multiple Vue Instances를 사용하여 vue 인스턴스 간의 상호작용에 대하여 학습하였습니다.

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-new Vue({
-  el: '#vue-app',
+var one = new Vue({
+  el: '#vue-app-one',
   data: {
-    health: 100,
-    ended: false
+   title: "vue-app-one"
   },
   methods: {
-    punch: function() {
-      this.health -= 10;
-      this.ended = this.health == 0;
-    },
-    restart: function() {
-      this.health = 100;
-      this.ended = false;
+    
+  },
+  computed: {
+    greet: function() {
+      return "Hello from app one";
+    }
+  }
+});
+var two = new Vue({
+  el: '#vue-app-two',
+  data: {
+   title: "vue-app-two"
+  },
+  methods: {
+    changeTitle: function() {
+      one.title = "Title changed";
     }
   },
   computed: {
+    greet: function() {
+      return "Yo dudes, this is app 2 speaking to ya :)";
+    }
   }
 });
+
+two.title = "changed from outside"
